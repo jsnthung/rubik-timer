af41e8cbfaead21a71f901477ef576c26ce2fd3a
Jason Thung <63440616+jsnthung@users.noreply.github.com>
2025-03-25 13:29:26 +0100

    installing tailwind v4.0

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="Container bg-red-500">
+        <h1>Hello tailwindCSSv4.0</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
