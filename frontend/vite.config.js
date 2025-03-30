b60acf473acfcb0b2804295569624bcb5b37b3d6
Jason Thung <63440616+jsnthung@users.noreply.github.com>
2025-03-30 18:08:18 +0200

    fixed cubing.js library for development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
+  optimizeDeps: {
+    exclude: ["cubing"], // Make sure it's not pre-bundled
+  },
 });
